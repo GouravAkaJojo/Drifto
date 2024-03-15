79c9d8c496e0e04b1eb31f1264d46a5c595c436d
Gourav <gourav98055@gmail.com>
2024-03-15 08:49:07 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import localfont from "next/font/local"
 const inter = Inter({ subsets: ["latin"] });
 
 const bionix = localfont({
-  src: "../../public/fonts/bionix.woff2",
+  src: "fonts/bionix.woff2",
   variable: "--font-bionix"
 })
 
